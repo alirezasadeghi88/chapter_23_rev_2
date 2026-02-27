7fbea66b54f1ccd49421d3e0fb0fd78b61806570
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-27 06:40:25 -0800

    add HeapSort

--- src/homework1/HeapSort.java
+++ src/homework1/HeapSort.java
@@ -1,4 +1,26 @@
 package homework1;
 
+import java.util.Comparator;
+
 public class HeapSort {
+    public static <E> void heapSort(E[] list) {
+        heapSort(list, (e1, e2) -> ((Comparable<E>)e1).compareTo(e2));
+    }
+
+    public static <E> void heapSort(E[] list, Comparator<E> c) {
+        Heap<E> heap = new Heap<>(c);
+
+        for (int i = 0; i < list.length; i++)
+            heap.add(list[i]);
+
+        for (int i = list.length - 1; i >= 0; i--)
+            list[i] = heap.remove();
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53};
+        heapSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
 }
